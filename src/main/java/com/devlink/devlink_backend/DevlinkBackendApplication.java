c30a5f038c4fc906b4f206f7bb935504996161bf
asalako <augustin-semevo.salako@etudiant.univ-rennes.fr>
2025-07-25 09:04:15 +0200

    DevLink project initializing ...

---
+++ src/main/java/com/devlink/devlink_backend/DevlinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devlink.devlink_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlinkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlink/devlink_backend/DevlinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlink.devlink_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
